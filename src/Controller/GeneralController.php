dca6a9ffa80b0c7ad48886cd1ee9c97945daf3a9
Fabian <FabianTsch@gmail.com>
2019-06-17 09:12:41 +0200

    Controller avec les premieres pages essentiel

--- src/Controller/GeneralController.php
+++ src/Controller/GeneralController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class GeneralController extends AbstractController
 {
     /**
-     * @Route("/", name="Accueil")
+     * @Route("/", name="home")
      */
     public function index()
     {
@@ -16,4 +16,76 @@ class GeneralController extends AbstractController
             'controller_name' => 'GeneralController',
         ]);
     }
+
+    /**
+     * @Route("/tarif", name="prices")
+     */
+    public function prices()
+    {
+        return $this->render('prices/index.html.twig', [
+            'controller_name' => 'GeneralController',
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'GeneralController',
+        ]);
+    }
+
+    /**
+     * @Route("/localisation", name="localization")
+     */
+    public function localization()
+    {
+        return $this->render('localization/index.html.twig', [
+            'controller_name' => 'GeneralController',
+        ]);
+    }
+
+    /**
+     * @Route("/evenements", name="events")
+     */
+    public function events()
+    {
+        return $this->render('events/index.html.twig', [
+            'controller_name' => 'GeneralController',
+        ]);
+    }
+
+    /**
+     * @Route("/evenement/{id}", name="event")
+     */
+    public function event($id)
+    {
+        return $this->render('event/index.html.twig', [
+            'controller_name' => 'GeneralController',
+        ]);
+    }
+
+    /**
+     * @Route("/creationEvenement", name="createEvent")
+     */
+    public function createEvents()
+    {
+        return $this->render('createEvents/index.html.twig', [
+            'controller_name' => 'GeneralController',
+        ]);
+    }
+
+    /**
+     * @Route("/lecentre", name="facilities")
+     */
+    public function facilities()
+    {
+        return $this->render('facilities/index.html.twig', [
+            'controller_name' => 'GeneralController',
+        ]);
+    }
+
+
 }
