cd9f5012e8578a0b0d632340d5231f755052a041
Madscientifique <36818408+Madscientifique@users.noreply.github.com>
2019-06-20 13:05:00 +0200

    présentation de la page évènement arrangée avec le bootstrapper et le css custom + ajouts de ressources

--- src/DataFixtures/EventFixtures.php
+++ src/DataFixtures/EventFixtures.php
@@ -44,7 +44,7 @@ class EventFixtures extends Fixture
                 $maintenant->add(new \DateInterval('P'.$compteurJour.'D'));
                 $datesEvenements=new DatesEvenements();
                 $datesEvenements->setDateDebut($maintenant);
-                $maintenant->add(new \DateInterval('PT6h'));
+                $maintenant->add(new \DateInterval('PT06H'));
                 $datesEvenements->setDateFin($maintenant);
                 $event->addDate($datesEvenements);
                 $compteurJour++;
