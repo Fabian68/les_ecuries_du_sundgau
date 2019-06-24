ddf13d3a5de7a901c686c9ab66332c08d3693d7f
Fabian <FabianTsch@gmail.com>
2019-06-24 17:50:23 +0200

    correctif fixture utilisateur

--- src/DataFixtures/UtilisateurFixtures.php
+++ src/DataFixtures/UtilisateurFixtures.php
@@ -6,9 +6,17 @@ use App\Entity\Galops;
 use App\Entity\Utilisateur;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UtilisateurFixtures extends Fixture
 {
+    private $passwordEncoder;
+
+     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+     {
+         $this->passwordEncoder = $passwordEncoder;
+     }
+
     public function load(ObjectManager $manager)
     {
         $galop = new Galops();
@@ -21,7 +29,10 @@ class UtilisateurFixtures extends Fixture
         $utilisateur->setNom('Tschirhart');
         $utilisateur->setPrenom('Fabian');
         $utilisateur->setEmail('FabianTsch@gmail.com');
-        $utilisateur->setMotDePasse('12345678');
+        $utilisateur->setMotDePasse($this->passwordEncoder->encodePassword(
+                         $utilisateur,
+                         '12345678'
+                     ));
         $utilisateur->setRoles(array('ROLE_USER'));
         $utilisateur->setGalop($galop);
         $manager->persist($utilisateur);
@@ -30,7 +41,11 @@ class UtilisateurFixtures extends Fixture
         $utilisateur->setNom('Blackquill');
         $utilisateur->setPrenom('Simon');
         $utilisateur->setEmail('Simon.blackquill@mail.fr');
-        $utilisateur->setMotDePasse('miaou123');
+        $utilisateur->setMotDePasse($this->passwordEncoder->encodePassword(
+            $utilisateur,
+            'miaou123'
+        ));
+        
         $utilisateur->setRoles(array('ROLE_ADMIN'));
         $utilisateur->setGalop( $galop);
         $manager->persist($utilisateur);
@@ -39,7 +54,11 @@ class UtilisateurFixtures extends Fixture
         $utilisateur->setNom('Smith');
         $utilisateur->setPrenom('John');
         $utilisateur->setEmail('John.Smith@yahoo.co.jp');
-        $utilisateur->setMotDePasse('lapomme123');
+        $utilisateur->setMotDePasse($this->passwordEncoder->encodePassword(
+            $utilisateur,
+            'lapomme123'
+        ));
+       
         $utilisateur->setRoles(array('ROLE_ADMIN_ASSO'));
         $utilisateur->setGalop( $galop);
         $manager->persist($utilisateur);
@@ -48,7 +67,11 @@ class UtilisateurFixtures extends Fixture
         $utilisateur->setNom('LaRoche');
         $utilisateur->setPrenom('Goron');
         $utilisateur->setEmail('GLR@wanadoo.fr');
-        $utilisateur->setMotDePasse('geode123');
+        $utilisateur->setMotDePasse($this->passwordEncoder->encodePassword(
+            $utilisateur,
+            'geode123'
+        ));
+        
         $utilisateur->setRoles(array('ROLE_USER'));
         $utilisateur->setGalop( $galop);
         $manager->persist($utilisateur);
