1f478dcddf9ffc9042fb187528d649690a928ac4
Fabian <FabianTsch@gmail.com>
2019-06-27 21:07:25 +0200

    go

--- src/Entity/Images.php
+++ src/Entity/Images.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ImagesRepository")
@@ -17,11 +18,6 @@ class Images
      * @ORM\Column(type="integer")
      */
     private $id;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $url;
 
     /**
      * NOTE: This is not a mapped field of entity metadata, just a simple property.
@@ -45,26 +41,9 @@ class Images
      */
     private $evenement;
 
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $updatedAt;
-
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getUrl(): ?string
-    {
-        return $this->url;
-    }
-
-    public function setUrl(string $url): self
-    {
-        $this->url = $url;
-
-        return $this;
     }
 
     public function getEvenement(): ?Event
@@ -80,12 +59,6 @@ class Images
     }
 
     /**
-    * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
-    * of 'UploadedFile' is injected into this setter to trigger the update. If this
-    * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
-    * must be able to accept an instance of 'File' as the bundle will inject one here
-    * during Doctrine hydration.
-    *
     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $imageFile
     */
    public function setImageFile(?File $imageFile = null): void
@@ -113,17 +86,7 @@ class Images
    {
        return $this->imageName;
    }
-
-   public function getUpdatedAt(): ?\DateTimeInterface
-   {
-       return $this->updatedAt;
-   }
-
-   public function setUpdatedAt(\DateTimeInterface $updatedAt): self
-   {
-       $this->updatedAt = $updatedAt;
-
-       return $this;
-   }
     
+
+
 }
