43ab643303969745286374ffe81bf9670cd11f6d
Fabian <FabianTsch@gmail.com>
2019-07-05 15:50:25 +0200

    debug pour laurine

--- src/Entity/Repas.php
+++ src/Entity/Repas.php
@@ -38,6 +38,7 @@ class Repas
     /**
      * @Groups("read")
      * @ORM\ManyToMany(targetEntity="App\Entity\Utilisateur", inversedBy="repas")
+     * @ORM\JoinTable(name="repas_utilisateur_cuisine")
      */
     private $cuisine;
 
