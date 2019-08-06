b4101be0cd2db27db29c25ef51196d38ef4bd74e
Madscientifique <36818408+Madscientifique@users.noreply.github.com>
2019-08-06 11:52:56 +0200

    arrangements supp de event au tailles d'écrans

--- src/Migrations/Version20190805132513.php
+++ src/Migrations/Version20190805132513.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190802081309 extends AbstractMigration
+final class Version20190805132513 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -31,13 +31,13 @@ final class Version20190802081309 extends AbstractMigration
         $this->addSql('CREATE TABLE event_galops (event_id INT NOT NULL, galops_id INT NOT NULL, INDEX IDX_E6B02A3871F7E88B (event_id), INDEX IDX_E6B02A386EA63067 (galops_id), PRIMARY KEY(event_id, galops_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE files_pdf (id INT AUTO_INCREMENT NOT NULL, file_name VARCHAR(255) DEFAULT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE galops (id INT AUTO_INCREMENT NOT NULL, niveau INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE images (id INT AUTO_INCREMENT NOT NULL, evenement_id INT DEFAULT NULL, image_name VARCHAR(255) DEFAULT NULL, updated_at DATETIME NOT NULL, INDEX IDX_E01FBE6AFD02F13 (evenement_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE images (id INT AUTO_INCREMENT NOT NULL, evenement_id INT NOT NULL, image_name VARCHAR(255) DEFAULT NULL, updated_at DATETIME NOT NULL, INDEX IDX_E01FBE6AFD02F13 (evenement_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE utilisateur (id INT AUTO_INCREMENT NOT NULL, galop_id INT NOT NULL, email VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, nom VARCHAR(255) NOT NULL, roles JSON NOT NULL, mot_de_passe VARCHAR(255) NOT NULL, date_naissance DATETIME NOT NULL, adresse VARCHAR(255) NOT NULL, telephone VARCHAR(255) NOT NULL, reset_token VARCHAR(255) DEFAULT NULL, updated_at DATETIME NOT NULL, image_name VARCHAR(255) DEFAULT NULL, validation_email_token VARCHAR(255) DEFAULT NULL, verified_mail TINYINT(1) DEFAULT NULL, INDEX IDX_1D1C63B34A6A299 (galop_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE participe (utilisateur_id INT NOT NULL, event_id INT NOT NULL, INDEX IDX_9FFA8D4FB88E14F (utilisateur_id), INDEX IDX_9FFA8D471F7E88B (event_id), PRIMARY KEY(utilisateur_id, event_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE utilisateur_event (utilisateur_id INT NOT NULL, event_id INT NOT NULL, INDEX IDX_FD6B5279FB88E14F (utilisateur_id), INDEX IDX_FD6B527971F7E88B (event_id), PRIMARY KEY(utilisateur_id, event_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE utilisateur_creneaux_benevoles (utilisateur_id INT NOT NULL, creneaux_benevoles_id INT NOT NULL, INDEX IDX_7D1BEE82FB88E14F (utilisateur_id), INDEX IDX_7D1BEE82C035FBBC (creneaux_benevoles_id), PRIMARY KEY(utilisateur_id, creneaux_benevoles_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE utilisateur_moyen_paiement_event (id INT AUTO_INCREMENT NOT NULL, utilisateur_id INT DEFAULT NULL, attribut_moyen_paiement_id INT DEFAULT NULL, event_id INT DEFAULT NULL, attribut_moyen_paiements_id INT DEFAULT NULL, utilisateurs_id INT DEFAULT NULL, events_id INT DEFAULT NULL, INDEX IDX_F823F1CFB88E14F (utilisateur_id), INDEX IDX_F823F1CA7823375 (attribut_moyen_paiement_id), INDEX IDX_F823F1C71F7E88B (event_id), INDEX IDX_F823F1CC7B9BAA5 (attribut_moyen_paiements_id), INDEX IDX_F823F1C1E969C5 (utilisateurs_id), INDEX IDX_F823F1C9D6A1065 (events_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE video (id INT AUTO_INCREMENT NOT NULL, evenement_id INT DEFAULT NULL, lien VARCHAR(255) NOT NULL, INDEX IDX_7CC7DA2CFD02F13 (evenement_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE utilisateur_moyen_paiement_event (id INT AUTO_INCREMENT NOT NULL, utilisateur_id INT DEFAULT NULL, attribut_moyen_paiement_id INT DEFAULT NULL, event_id INT DEFAULT NULL, INDEX IDX_F823F1CFB88E14F (utilisateur_id), INDEX IDX_F823F1CA7823375 (attribut_moyen_paiement_id), INDEX IDX_F823F1C71F7E88B (event_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE video (id INT AUTO_INCREMENT NOT NULL, evenement_id INT NOT NULL, lien VARCHAR(255) NOT NULL, INDEX IDX_7CC7DA2CFD02F13 (evenement_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE creneaux_benevoles ADD CONSTRAINT FK_B135227271F7E88B FOREIGN KEY (event_id) REFERENCES event (id)');
         $this->addSql('ALTER TABLE dates_evenements ADD CONSTRAINT FK_A00387DD71F7E88B FOREIGN KEY (event_id) REFERENCES event (id)');
         $this->addSql('ALTER TABLE event_galops ADD CONSTRAINT FK_E6B02A3871F7E88B FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE');
@@ -53,9 +53,6 @@ final class Version20190802081309 extends AbstractMigration
         $this->addSql('ALTER TABLE utilisateur_moyen_paiement_event ADD CONSTRAINT FK_F823F1CFB88E14F FOREIGN KEY (utilisateur_id) REFERENCES utilisateur (id)');
         $this->addSql('ALTER TABLE utilisateur_moyen_paiement_event ADD CONSTRAINT FK_F823F1CA7823375 FOREIGN KEY (attribut_moyen_paiement_id) REFERENCES attribut_moyen_paiements (id)');
         $this->addSql('ALTER TABLE utilisateur_moyen_paiement_event ADD CONSTRAINT FK_F823F1C71F7E88B FOREIGN KEY (event_id) REFERENCES event (id)');
-        $this->addSql('ALTER TABLE utilisateur_moyen_paiement_event ADD CONSTRAINT FK_F823F1CC7B9BAA5 FOREIGN KEY (attribut_moyen_paiements_id) REFERENCES attribut_moyen_paiements (id)');
-        $this->addSql('ALTER TABLE utilisateur_moyen_paiement_event ADD CONSTRAINT FK_F823F1C1E969C5 FOREIGN KEY (utilisateurs_id) REFERENCES utilisateur (id)');
-        $this->addSql('ALTER TABLE utilisateur_moyen_paiement_event ADD CONSTRAINT FK_F823F1C9D6A1065 FOREIGN KEY (events_id) REFERENCES event (id)');
         $this->addSql('ALTER TABLE video ADD CONSTRAINT FK_7CC7DA2CFD02F13 FOREIGN KEY (evenement_id) REFERENCES event (id)');
     }
 
@@ -65,7 +62,6 @@ final class Version20190802081309 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE utilisateur_moyen_paiement_event DROP FOREIGN KEY FK_F823F1CA7823375');
-        $this->addSql('ALTER TABLE utilisateur_moyen_paiement_event DROP FOREIGN KEY FK_F823F1CC7B9BAA5');
         $this->addSql('ALTER TABLE utilisateur_creneaux_benevoles DROP FOREIGN KEY FK_7D1BEE82C035FBBC');
         $this->addSql('ALTER TABLE creneaux_benevoles DROP FOREIGN KEY FK_B135227271F7E88B');
         $this->addSql('ALTER TABLE dates_evenements DROP FOREIGN KEY FK_A00387DD71F7E88B');
@@ -74,7 +70,6 @@ final class Version20190802081309 extends AbstractMigration
         $this->addSql('ALTER TABLE participe DROP FOREIGN KEY FK_9FFA8D471F7E88B');
         $this->addSql('ALTER TABLE utilisateur_event DROP FOREIGN KEY FK_FD6B527971F7E88B');
         $this->addSql('ALTER TABLE utilisateur_moyen_paiement_event DROP FOREIGN KEY FK_F823F1C71F7E88B');
-        $this->addSql('ALTER TABLE utilisateur_moyen_paiement_event DROP FOREIGN KEY FK_F823F1C9D6A1065');
         $this->addSql('ALTER TABLE video DROP FOREIGN KEY FK_7CC7DA2CFD02F13');
         $this->addSql('ALTER TABLE event_galops DROP FOREIGN KEY FK_E6B02A386EA63067');
         $this->addSql('ALTER TABLE utilisateur DROP FOREIGN KEY FK_1D1C63B34A6A299');
@@ -82,7 +77,6 @@ final class Version20190802081309 extends AbstractMigration
         $this->addSql('ALTER TABLE utilisateur_event DROP FOREIGN KEY FK_FD6B5279FB88E14F');
         $this->addSql('ALTER TABLE utilisateur_creneaux_benevoles DROP FOREIGN KEY FK_7D1BEE82FB88E14F');
         $this->addSql('ALTER TABLE utilisateur_moyen_paiement_event DROP FOREIGN KEY FK_F823F1CFB88E14F');
-        $this->addSql('ALTER TABLE utilisateur_moyen_paiement_event DROP FOREIGN KEY FK_F823F1C1E969C5');
         $this->addSql('DROP TABLE attribut_moyen_paiements');
         $this->addSql('DROP TABLE creneaux_benevoles');
         $this->addSql('DROP TABLE dates_evenements');
